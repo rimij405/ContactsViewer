6de997613becfcc613e4ef669a18030cb48120c4
Ian Effendi <effendiian+github@gmail.com>
2016-06-14 15:20:04 -0400

    Overview: Fixed inconsistencies across .css files. Editing the final site .css should consist of editing the Site.css file.

--- ContactConnectionApplication/ContactConnectionApplication/App_Start/BundleConfig.cs
+++ ContactConnectionApplication/ContactConnectionApplication/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace ContactConnectionApplication
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js"));
 
+			bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+						"~/Scripts/knockout-{version}.js"));
+
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -18,10 +21,13 @@ namespace ContactConnectionApplication
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
+					  "~/Scripts/materialize.js",
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
+					  "~/Content/animate.css",
 					  "~/Content/bootstrap.css",
+					  "~/Content/materialize.css",
 					  "~/Content/site.css"));
 		}
 	}
--- ContactConnectionApplication/ContactConnectionApplication/Scripts/_references.js
+++ ContactConnectionApplication/ContactConnectionApplication/Scripts/_references.js
@@ -2,3 +2,7 @@
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery-1.9.1.js" />
 /// <reference path="jquery-1.9.1.min.js" />
+/// <reference path="knockout-3.4.0.js" />
+/// <reference path="materialize.js" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="respond.js" />
